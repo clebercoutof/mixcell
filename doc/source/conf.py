385d37501a7d652818b44588042ced44610e9b98
clebercoutof <clebercoutof@gmail.com>
2017-04-24 14:52:08 -0300

    doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,6 +18,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -82,7 +83,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -151,6 +153,3 @@ texinfo_documents = [
      author, 'Mixcell', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
