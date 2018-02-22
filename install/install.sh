4742aaa5abbaafb86965e9d03dc339b7a1b88dc3
Cleber Couto Filho <clebercoutof@gmail.com>
2018-02-22 15:29:36 -0300

    Change export from .profile to .bashrc

--- install/install.sh
+++ install/install.sh
@@ -6,21 +6,24 @@ rm -rf 3.4.3.tar.gz
 cd ..
 cd QTCode/
 cwd=$(pwd)
-echo export PATH=\$PATH:$(pwd) >>~/.profile
+echo export PATH=\$PATH:$(pwd) >>~/.bashrc
 chmod +x mixcell_qt.py 
 ln -s mixcell_qt.py mixcell
 chmod +x mixcell
-source ~/.profile
+source ~/.bashrc
 cd ..
 cd source_code/
 cwd=$(pwd)
-echo export PYTHONPATH=\$PYTHONPATH:$(pwd) >>~/.profile
+echo export PYTHONPATH=\$PYTHONPATH:$(pwd) >>~/.bashrc
+source ~/.bashrc
 apt-cache search pyqt
 sudo apt-get install python-qt4
 cd .. 
 cd install/DynamixelSDK-3.4.3/c/build/linux64/
 make
 sudo make install
-
+cd -
+cd install/
+rm -rf DynamixelSDK-3.4.3/
 
 
